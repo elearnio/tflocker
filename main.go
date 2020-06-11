5a591e93c7140520b92d3f522c5704c712f3bcab
Christoph Mertz <cmertz@users.noreply.github.com>
2020-06-11 14:04:50 +0200

    actually use the correct lock info

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 	lockInfo := state.NewLockInfo()
 	lockInfo.Operation = operation
 
-	lockID, err := s.Lock(state.NewLockInfo())
+	lockID, err := s.Lock(lockInfo)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
